0ab6ebbcbd837db7ef9910e4404f70440011c126
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-04-26 11:19:54 +0300

    xml

--- app/src/main/java/com/ideasapp/lovetimecapsule/presentation/MainActivity.kt
+++ app/src/main/java/com/ideasapp/lovetimecapsule/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.ideasapp.lovetimecapsule
+package com.ideasapp.lovetimecapsule.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ideasapp.lovetimecapsule.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState:Bundle?) {
