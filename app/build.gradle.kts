f37058277885cb47a1c58d74b50f46697d87330c
BogdanDohtarenko <bogdandohtarenko@gmail.com>
2025-05-06 10:03:49 +0300

    kapt problems have been  solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
@@ -38,8 +38,8 @@ android {
 
 dependencies {
     implementation(libs.rxjava3.rxjava)
-    implementation(libs.androidx.room.runtime)
-    kapt(libs.androidx.room.compiler.v271)
+    implementation("androidx.room:room-runtime:2.5.2")
+    kapt("androidx.room:room-compiler:2.5.2")
     implementation(libs.androidx.room.rxjava3)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
